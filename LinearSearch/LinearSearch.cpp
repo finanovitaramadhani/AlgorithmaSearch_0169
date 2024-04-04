11b198adf76abdcb8f60bb6ce77e84d3feb1ef7b
finanovita <finanovita479@gmail.com>
2024-04-04 10:48:31 +0700

    membuat prosedur input

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -8,6 +8,11 @@ int arr[20];    // Array to be searched
 int n;          // Number of elements in the array
 int i;          // Index of array elements
 
+void input()
+{
+
+}
+
 int main()
 {
     
